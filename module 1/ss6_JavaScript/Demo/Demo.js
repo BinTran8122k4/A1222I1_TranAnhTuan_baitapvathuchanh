fe011f2e68ef2c7e93b92fb0163d1180ad518772
unknown <tbin08122004@gmail.com>
2023-02-06 21:30:12 +0700

    nopbaitapss7

---
+++ module 1/ss6_JavaScript/Demo/Demo.js
@@ -0,0 +1,17 @@
+var intro = prompt('Bạn tên là gì ?')
+    alert('Chào' + intro);
+let ques = confirm('do u who am i ? ')
+if(ques === true){
+    alert("Tôi có Biết Bạn")
+}
+else {
+   alert('Tôi Không Biết Bạn')
+}
+
+
+
+
+
+
+
+
